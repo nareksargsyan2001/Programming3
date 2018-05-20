a8abe5d06081c30cc84537247ba9f861c36c4a96
Your Name <you@example.com>
2018-05-20 12:21:37 +0400

    serveri ave

---
+++ lesson5/script.js
@@ -0,0 +1,32 @@
+function main() {
+   var socket = io.connect('http://localhost:3000');
+   var chatDiv = document.getElementById('chat');
+   var input = document.getElementById('message');
+   var button = document.getElementById('submit');
+   var button1 = document.getElementById('delete');
+
+   function handleSubmit(evt) {
+       var val = input.value;
+       if (val != "") {
+           socket.emit("send message", val);
+       }
+   }
+   button.onclick = handleSubmit;
+   function handleMessage(msg) {
+   		var p = document.createElement('p');
+   		p.innerText = msg;
+   		chatDiv.appendChild(p);
+   		input.value = "";
+}
+   button1.onclick = handleDelete;
+   function handleMessage(msg) {
+   		var p = document.createElement('p');
+   		p.innerText = msg;
+   		chatDiv.appendChild(p);
+   		input.value = "";
+}
+
+socket.on('display message', handleMessage);
+} // main closing bracket
+
+window.onload = main;
